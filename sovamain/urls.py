10d95fc0a8512829af45d8c780da87a2f4eeb61d
Vovsla <GitP@ss1!>
2023-06-21 20:22:30 +0300

    add app

--- sovamain/urls.py
+++ sovamain/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 
+from .views import sova_index
+
+app_name = 'sovamain'
 urlpatterns = [
-    ]
+    path("", sova_index, name="main"),
+]
--- sovamain/views.py
+++ sovamain/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def sova_index(request: HttpRequest):
+    return HttpResponse("привет")
